264070b785cc99de77b296ee3768a44ffa075b98
Sujeet4gyantiraj <sujeet4gyantiraj@gmail.com>
2026-03-24 14:22:54 +0530

    gdd

--- t_risk_detection.py
+++ t_risk_detection.py
@@ -1,7 +1,7 @@
 # u_risk_detection.py
 import logging
 from s_ai_model import run_llm
-from pdf_ai_review.backupfiles.t_utils import extract_json_from_text
+from backupfiles.t_utils import extract_json_from_text
 
 logger = logging.getLogger(__name__)
 
